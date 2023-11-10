8cd16f2eb14305e9dde973dc535d56a22c03df63
robi <robins07@me.com>
2023-11-10 18:27:10 +0100

    update zkapp address

--- store/sequence/sequence.index.ts
+++ store/sequence/sequence.index.ts
@@ -1,22 +1,24 @@
 // following this example https://github.com/productdevbook/oku-nuxt3-template/tree/master/src
 import { defineStore } from "pinia";
-import type { SequenceState } from '~/store/sequence/sequence.types';
+import type { SequenceState } from "~/store/sequence/sequence.types";
 
 export const useSequence = defineStore("sequence", {
   state: (): SequenceState => ({
-    bruteForceVerifierAddress: 'B62qoB21FbxU4mCmz5vUSuMTfAqPZYeKt7vy9TKNxbeACNu4TaCgPJo',
-    segmentVerifierAddress: 'B62qjxuF5zqUvdHX8SKCLnGRvELSqSLqoJFh8c3AVFaLikGhuTWaA3K',
-    sequence: '',
-    pattern: '',
-    proof: ''
+    bruteForceVerifierAddress:
+      "B62qpWAr1nJ6WNCZnAUsWPXPAhT9nDNRQAZvuM1vQ6gDjgQKCqCMPxr",
+    segmentVerifierAddress:
+      "B62qjxuF5zqUvdHX8SKCLnGRvELSqSLqoJFh8c3AVFaLikGhuTWaA3K",
+    sequence: "",
+    pattern: "",
+    proof: "",
   }),
   actions: {
     computeProof: async function () {
-        this.proof = 'fakeproof'
-        console.log('computeProof')
+      this.proof = "fakeproof";
+      console.log("computeProof");
     },
     verify: async function () {
-        console.log('verify')
+      console.log("verify");
     },
-  }
+  },
 });
