0255316604c85e5e46f232f329178010651fae3a
robi <robins07@me.com>
2024-01-30 18:54:02 +0100

    :sparkles: Navbar cart

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,50 +1,86 @@
 <template>
-    <div class="p-4">
-      <div class="flex gap-4"> <!-- Flex container -->
-        <!-- Connect Wallet -->
-        <ConnectWallet></ConnectWallet>
-  
-        <!-- Theme Selector -->
-        <select class="select w-full max-w-xs" v-model="colorMode.preference">
-          <option disabled selected>Theme</option>
-          <option v-for="theme in themes" :key="theme">{{ theme }}</option>
-        </select>
+  <div class="p-4">
+    <div class="flex gap-4">
+      <!-- Flex container -->
+      <!-- Connect Wallet -->
+      <ConnectWallet></ConnectWallet>
+
+      <!-- Theme Selector -->
+      <select class="select w-full max-w-xs" v-model="colorMode.preference">
+        <option disabled selected>Theme</option>
+        <option v-for="theme in themes" :key="theme">{{ theme }}</option>
+      </select>
+      <div class="navbar-end">
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <div class="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span class="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          <div
+            tabindex="0"
+            class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+          >
+            <div class="card-body">
+              <span class="font-bold text-lg">8 Items</span>
+              <span class="text-info">Subtotal: $999</span>
+              <div class="card-actions">
+                <button class="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </template>
-  <script setup lang="ts">
-  import ConnectWallet from "../wallet/ConnectWallet.vue";
-  const colorMode = useColorMode();
-  const themes = [
-    "system",
-    "light",
-    "dark",
-    "cupcake",
-    "bumblebee",
-    "emerald",
-    "corporate",
-    "synthwave",
-    "retro",
-    "cyberpunk",
-    "valentine",
-    "halloween",
-    "garden",
-    "forest",
-    "aqua",
-    "lofi",
-    "pastel",
-    "fantasy",
-    "wireframe",
-    "black",
-    "luxury",
-    "dracula",
-    "cmyk",
-    "autumn",
-    "business",
-    "acid",
-    "lemonade",
-    "night",
-    "coffee",
-    "winter",
-  ];
-  </script>
+  </div>
+</template>
+<script setup lang="ts">
+import ConnectWallet from "../wallet/ConnectWallet.vue";
+const colorMode = useColorMode();
+const themes = [
+  "system",
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+  "aqua",
+  "lofi",
+  "pastel",
+  "fantasy",
+  "wireframe",
+  "black",
+  "luxury",
+  "dracula",
+  "cmyk",
+  "autumn",
+  "business",
+  "acid",
+  "lemonade",
+  "night",
+  "coffee",
+  "winter",
+];
+</script>
